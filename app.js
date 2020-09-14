ac039d3391ab53031413d4c03c3e87012c5baacb
MahmudulIslamSiman <mahmudulislamsiman@gmail.com>
2020-09-14 21:39:24 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+Vue.component('like', {
+	template: "<div class='like-data float-right text-white'><button class='icon-rocknroll mr-1 p-0 border-0' v-class='active: liked' v-on='click: toggleLike'><i class='fa fa-thumbs-up text-white' aria-hidden='true'></i></button><span class='like-count' v-class='active: liked'>{{ likesCount }}</span></div>",
+	data: function() {
+		return {
+			liked: false,
+			likesCount: 0
+		}
+	},
+	methods: {
+		toggleLike: function() {
+			this.liked = !this.liked;
+			this.liked ? this.likesCount++ : this.likesCount--;
+		}
+	}
+});
+new Vue({
+	el: '.comments-main',
+});
